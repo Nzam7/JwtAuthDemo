047fbf3398da9d6cc6b8c3378f8746d72ed91543
Nzam7 <pistis.nzam@gmail.com>
2024-06-20 02:07:19 +0200

    Initial commit with JWT authentication template

---
+++ JwtAuthDemo/Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtAuthDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] UserLogin model)
+        {
+            // In production, we would validate the user credentials from a database
+            if (model.Username == "test" && model.Password == "password")
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                // Retrieve the key from the configuration
+                var key = _configuration.GetSection("JwtSettings:SecretKey").Value;
+                var keyBytes = Encoding.ASCII.GetBytes(key);
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                    new Claim(ClaimTypes.Name, model.Username)
+                    }),
+                    Expires = DateTime.UtcNow.AddHours(1),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(keyBytes), SecurityAlgorithms.HmacSha256Signature)
+                };
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                var tokenString = tokenHandler.WriteToken(token);
+
+                return Ok(new { Token = tokenString });
+            }
+
+            return Unauthorized();
+        }
+    }
+
+
+    public class UserLogin
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ JwtAuthDemo/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace JwtAuthDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        [Authorize]
+        public IActionResult Get()
+        {
+            return Ok("This is a secured endpoint.");
+        }
+    }
+}
